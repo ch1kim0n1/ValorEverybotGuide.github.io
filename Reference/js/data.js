7ad23c737be44a3653242ed886d6e36f1646f7b9
Vlad K <110909601+ch1kim0n1@users.noreply.github.com>
2023-02-16 21:48:05 -0600

    Initial Commit

---
+++ Reference/js/data.js
@@ -0,0 +1,118 @@
+function getDatach1kim0n1() {
+    const Status = {
+        Red: "red",
+        Blue: "blue"
+
+    }
+
+    class Title {
+        id;
+        title;
+        count;
+        isShowing;
+
+        constructor(id, title) {
+            this.id = id;
+            this.title = title;
+            this.count = 0;
+            this.isShowing = false;
+        }
+    }
+
+    let ch1kim0n1 = {
+        Side: [
+            {
+                nickname: "SteveDekart",
+                image: "images/users/ST.jpg",
+                url: "https://stepan323446.github.io/anime-achievment-100days/",
+                status: Status.Current
+            },
+            {
+                nickname: "GreenDavid004",
+                image: "images/users/david.jpg",
+                url: "https://davidk004.github.io/100-day-challenge/",
+                status: Status.Current
+            },
+            {
+                nickname: "IamVoid",
+                image: "images/users/IamVoid.png",
+                url: "https://anilist.co/user/IamVoid/",
+                status: Status.Current
+            },
+            {
+                nickname: "F41NZ",
+                image: "images/users/F41NZ.jpg",
+                url: "https://anilist.co/user/F41NZ/",
+                status: Status.Current
+            },
+            {
+                nickname: "BommieBomBom",
+                image: "images/users/BommieBomBom.jpg",
+                url: "https://anilist.co/user/BommieBomBom",
+                status: Status.Current
+            },
+            {
+                nickname: "sololol",
+                image: "images/users/sololol.jpg",
+                url: "https://anilist.co/user/sololol",
+                status: Status.Current
+            },
+            {
+                nickname: "bokut0",
+                image: "images/users/bokut0.png",
+                url: "https://anilist.co/user/bokut0",
+                status: Status.Current
+            },
+            {
+                nickname: "keiikeii",
+                image: "images/users/keiikeii.png",
+                url: "https://anilist.co/user/keiikeii/",
+                status: Status.Current
+            },
+            {
+                nickname: "stephonee",
+                image: "images/users/stephonee.png",
+                url: "https://anilist.co/user/stephonee",
+                status: Status.Current
+            },
+            {
+                nickname: "RossyNarukami",
+                image: "images/users/RossyHarukami.png",
+                url: "https://anilist.co/activity/507121677",
+                status: Status.Failed
+            },
+            {
+                nickname: "Ani-Dotes",
+                image: "images/users/dotes.jpg",
+                url: "https://anidotesblog.wordpress.com/100-day-anime-challenge/",
+                status: Status.Failed
+            },
+            {
+                nickname: "Celestial Sparkles",
+                image: "images/users/CelestialSparkles.png",
+                url: "https://howanimestuffworks176744600.wordpress.com/category/anime-challenges-events/100-days-of-anime-challenge/",
+                status: Status.Failed
+            },
+            {
+                nickname: "GHAUR1",
+                image: "images/users/Ghaur1.png",
+                url: "https://anilist.co/activity/474754966",
+                status: Status.Complete
+            },
+            {
+                nickname: "OskarL",
+                image: "images/users/OskarL.jpg",
+                url: "https://anilist.co/activity/478516794",
+                status: Status.Complete
+            },
+            {
+                nickname: "Karandi",
+                image: "images/users/Karandi.webp",
+                url: "https://100wordanime.blog/category/30-day-anime-challenge/",
+                status: Status.Complete
+            }
+        ],
+    }
+
+return ch1kim0n1;
+}
---
+++ js/main.js
@@ -0,0 +1,43 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+ // Get the bordered text element and tab content elements
+const borderedText = document.querySelector('.bordered-text');
+const tabContent = document.querySelectorAll('.tabcontent');
+
+// Add an event listener to each tab button
+document.querySelectorAll('.tablinks').forEach(tabButton => {
+  tabButton.addEventListener('click', (event) => {
+    // Remove the 'show' class from the bordered text element
+    borderedText.classList.remove('show');
+    // Add the 'hide' class to the bordered text element
+    borderedText.classList.add('hide');
+    
+    // Loop through each tab content element
+    tabContent.forEach(content => {
+      // If the content element's ID matches the tab button's target
+      if (content.id === event.target.dataset.target) {
+        // Add the 'active' class to the tab button
+        tabButton.classList.add('active');
+        // Display the tab content element
+        content.style.display = 'block';
+      } else {
+        // Remove the 'active' class from the tab button
+        tabButton.classList.remove('active');
+        // Hide the tab content element
+        content.style.display = 'none';
+      }
+    });
+  });
+});
